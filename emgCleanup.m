3e8c9c92aef92c8bedfa161fb056da443bd9020e
Shantanu Ray <chico.chego@gmail.com>
2018-08-09 14:24:25 +0800

    Removed redirection to filter specs. Present in path

--- emgCleanup.m
+++ emgCleanup.m
@@ -1,6 +1,6 @@
 function emg_clean = emgCleanup(emg_signal)
 % emg_clean = emgCleanup(emg_signal);
-load('/Users/ayesha/Projects/Electrophysiology/EMG/bandpass_filter_butterworth_10-350Hz.mat');
+load('bandpass_filter_butterworth_10-350Hz.mat');
 
 emg_ch1_filtered = filter(d, emg_signal);
 % Applies a digital filter, defined using the matlab filter app, bandpass 10-500hz, butterworth.
