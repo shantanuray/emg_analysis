b81e4793fb361be95e871d42fbee3d4ba54bf23f
Shantanu Ray <chico.chego@gmail.com>
2018-08-09 14:25:28 +0800

    Functionalized edf_analyze

---
+++ emgRetrieve.m
@@ -0,0 +1,27 @@
+function emgData = emgRetrieve(emgFile, timestampStartStop,paddingDuration,windowDuration,samplingFrequency)
+  %defines a variable for the path of the file to be opened 
+  [hdr, record] = edfread(emgFile);
+  % the program expects two outputs the header(hdr) and the data(volts) which is in rows, each channel is recorded in one row
+  for i = 1:size(record,1)
+    
+    % y=fft(emg_recording_ch1);
+    % m=abs(y);
+    % p=angle(y);
+    % f = (0:length(y)-1)*(1/samplingFrequency);
+    % plot(f,m)
+    % this code is written as an optional check using the Fast fourier transform, 
+    %it is useful once the filter is applied to look at data before and after the butterworth filter
+
+
+    % Applies a digital filter, defined using the matlab filter app, bandpass 10-500hz, butterworth.
+    % and rectifies bipolar emg signals
+    record(i,:) = emgCleanup(record(i,:));
+
+    % Select samples in the emg channel data that corresponds to the time stamp from the annotations data file
+    % Samples are chosen paddingDuration  before and after time stamp 
+
+    emgData(i,:) = emgSelect(record(i,:), timestampStartStop, paddingDuration, samplingFrequency);
+
+    %computes the moving average for a window of 25 ms or 0.025s, useful to find gradual trends in data
+    emgData(i,:) = movingAverage(emgData(i,:), windowDuration, samplingFrequency);
+  end;
