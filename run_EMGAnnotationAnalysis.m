cb19a3e20b51c38f51bf442f4a52646a944bbfd6
Ayesha Thanawalla <ayeshat90@gmail.com>
2018-08-10 11:59:34 +0800

    Updated wrt reach and fixed window

--- run_EMGAnnotationAnalysis.m
+++ run_EMGAnnotationAnalysis.m
@@ -1,8 +1,11 @@
-annotationFile = '/Users/chico/Downloads/emg/18LLR_OFF_annotations.xlsx';
-[emgPathName, annotationFilename] = fileparts(annotationFile);
+fname = {'18LLR_OFF_annotations.xlsx',...
+	'22UN_OFF_annotations.xlsx',...
+	'18LLR_ONG_annotations.xlsx',...
+	'18LLR_ONI_annotations.xlsx',...
+	'22UN_ONG_annotations.xlsx',...
+	'22UN_ONI_annotations.xlsx'};
 
-annotations = readtable(annotationFile);
-emgCounterReference = readtable('/Users/chico/Downloads/emg/annotations_18LLR_22UN_Master.txt');
+fname = {'18LLR_OFF_20180606_annotations.xlsx'};
 
 emgSamplingFrequency = 2000; %Hz
 videoSamplingFrequency = 25; %Hz
@@ -10,26 +13,60 @@ padding = 0.5; % 0.5s
 movingAverageWindow = 25/1000; % 25ms
 baselineEnd = 0.2; % 0.2s
 
-[emgData,timestampEMG] = emgExtractFromAnnotations(annotations,emgCounterReference,emgPathName,emgSamplingFrequency,videoSamplingFrequency,padding);
-save(fullfile(emgPathName,[annotationFilename,'_RawEMG_All.mat']),'emgData');
 
-[emgDataPlot,timestampEMG,xPlot] = emgDataWindow(emgData,annotations,timestampEMG,emgSamplingFrequency);
-save(fullfile(emgPathName,[annotationFilename,'_RawEMG_TimeWindow.mat']),'emgDataPlot');
+emgCounterReference = readtable('/Users/ayesha/Documents/Ayesha_phd_local storage/EMG data analysis/emg_data_files/annotations_18LLR_22UN_Master.txt');
 
-emgAnalyzed = emgDataAnalysis(emgDataPlot,emgSamplingFrequency,baselineEnd,movingAverageWindow);
-save(fullfile(emgPathName,[annotationFilename,'_AnalyzedEMG.mat']),'emgAnalyzed');
+for f = 1:length(fname)
+	annotationFile = fullfile('/Users/ayesha/Documents/Ayesha_phd_local storage/EMG data analysis/emg_data_files/',fname{f});
+	[emgPathName, annotationFilename] = fileparts(annotationFile);
+	annotations = readtable(annotationFile);
+	% [emgData,timestampEMG] = emgExtractFromAnnotations(annotations,emgCounterReference,emgPathName,emgSamplingFrequency,videoSamplingFrequency,padding);
 
-h=figure;
-subplot(3,1,1)
-plot(xPlot(201:end),emgAnalyzed.Average(201:end,1),'b-')
-hold on
-plot(xPlot(201:end),emgAnalyzed.MovingAverage(201:end,1),'r-')
-subplot(3,1,2)
-plot(xPlot(201:end),emgAnalyzed.Average(201:end,2),'b-')
-hold on
-plot(xPlot(201:end),emgAnalyzed.MovingAverage(201:end,2),'r-')
-subplot(3,1,3)
-plot(xPlot(201:end),emgAnalyzed.Average(201:end,3),'b-')
-hold on
-plot(xPlot(201:end),emgAnalyzed.MovingAverage(201:end,3),'r-')
-hold on
+	Dates = unique(annotations.Date);
+	Animal = unique(annotations.Animal);
+	emgData = [];
+	timestampEMG.Counter = [];
+	timestampEMG.Reach = [];
+	timestampEMG.StartStop = [];
+	timestampEMG.StartStopNormalized = [];
+	for i = 1:length(Animals)
+		for j = 1:length(uniqueDates)
+			% Assumption emg filename is export_18LLR_20180622.edf
+			emgFilename = fullfile(emgPathName,...
+			  ['export_',Animals{j},'_',num2str(Dates(i)),'.edf']);
+			emgDataAll = emgRetrieve(emgFilename);
+			indx = find(strcmpi(annotations.Animal,Animals(i)) & annotations.Date==Dates(j));
+
+
+			% Select samples in the emg channel data that corresponds to the time stamp from the annotations data file
+			% Samples are chosen paddingDuration  before and after time stamp 
+			[emgData_,timestampEMG_] = emgExtractFromReach(emgDataAll,annotations(indx,:),emgCounterReference,videoSamplingFrequency,emgSamplingFrequency,padding);
+			emgData = cat(2,emgData,emgData_);
+			timestampEMG.Counter = [timestampEMG.Counter,timestampEMG_.Counter];
+			timestampEMG.Reach = [timestampEMG.Reach,timestampEMG_.Reach];
+			timestampEMG.StartStop = [timestampEMG.StartStop,timestampEMG_.StartStop];
+			timestampEMG.StartStopNormalized = [timestampEMG.StartStopNormalized;timestampEMG_.StartStopNormalized];
+			save(fullfile(emgPathName,[animal(i),'_',uniqueDates(j),'_RawEMG_All.mat']),'emgDataAll','emgData');
+
+	% [emgDataPlot,timestampEMG,xPlot] = emgDataWindow(emgData,annotations,timestampEMG,emgSamplingFrequency);
+	% save(fullfile(emgPathName,[annotationFilename,'_RawEMG_TimeWindow.mat']),'emgDataPlot');
+
+	% emgDataPlot = cell2mat(emgData);
+	emgAnalyzed = emgDataAnalysisOld(emgData,emgSamplingFrequency,baselineEnd,movingAverageWindow);
+	save(fullfile(emgPathName,[annotationFilename,'_AnalyzedEMG.mat']),'emgAnalyzed');
+
+	h=figure;
+	subplot(3,1,1)
+	plot((0:size(emgAnalyzed.Average,3)-1)/emgSamplingFrequency,reshape(emgAnalyzed.Average(1,1,:),1,2001))
+	hold on
+	plot((0:size(emgAnalyzed.Average,3)-1)/emgSamplingFrequency,reshape(emgAnalyzed.MovingAverage(1,1,:),1,2001),'r')
+	subplot(3,1,2)
+	plot((0:size(emgAnalyzed.Average,3)-1)/emgSamplingFrequency,reshape(emgAnalyzed.Average(2,1,:),1,2001))
+	hold on
+	plot((0:size(emgAnalyzed.Average,3)-1)/emgSamplingFrequency,reshape(emgAnalyzed.MovingAverage(2,1,:),1,2001),'r')
+	subplot(3,1,3)
+	plot((0:size(emgAnalyzed.Average,3)-1)/emgSamplingFrequency,reshape(emgAnalyzed.Average(3,1,:),1,2001))
+	hold on
+	plot((0:size(emgAnalyzed.Average,3)-1)/emgSamplingFrequency,reshape(emgAnalyzed.MovingAverage(3,1,:),1,2001),'r')
+	hold off
+end
