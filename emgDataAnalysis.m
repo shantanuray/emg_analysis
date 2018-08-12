a8ba6d08267df98ba1f33536d261fb775eaa3eb9
Shantanu Ray <chico.chego@gmail.com>
2018-08-12 16:38:03 +0800

    Added baseline. Updated analysis. Write to table

--- emgDataAnalysis.m
+++ emgDataAnalysis.m
@@ -1,22 +1,18 @@
-function emgAnalyzed = emgDataAnalysis(emgRawData,emgSamplingFrequency,timestampEMG)
+function emgAnalyzed = emgDataAnalysis(emgRawData,emgBaseline,emgSamplingFrequency,timestampEMG)
 
-  baselineEnd=0.2;
+  activityStart=0.2;
+  activityEnd=0.6;
   movingAverageWindow=25/1000;
   removewindow=0.1;
   % Remove window of 0.1
   removewindow = round(removewindow*emgSamplingFrequency);
 
-
   emgAnalyzed.RawData = emgRawData(:,:,removewindow+1:size(emgRawData,3)-removewindow);
   % Compute baseline for each row from 0th to 0.2 of the time window
-  baselineStartSamples = 0*emgSamplingFrequency+1;
-  baselineEndSamples = round(baselineEnd*emgSamplingFrequency);
-
-  emgAnalyzed.Baseline = emgAnalyzed.RawData(:,:,int16(baselineStartSamples:baselineEndSamples));
-  emgAnalyzed.Baseline = mean(emgAnalyzed.Baseline,3);
+  emgAnalyzed.Baseline = mean(emgBaseline,2);
 
   % Compute mVolt activity within the reach
-  activityStart = baselineEndSamples+1;
+  activityStart = round(activityStart*emgSamplingFrequency)+1;
 
   emgAnalyzed.Active = emgAnalyzed.RawData(:,:,int16(activityStart:end));
   emgAnalyzed.Active = mean(emgAnalyzed.Active,3);
@@ -35,40 +31,20 @@ function emgAnalyzed = emgDataAnalysis(emgRawData,emgSamplingFrequency,timestamp
       initpos = reachpos+round((timestampEMG.Initialize(j)-timestampEMG.Reach(j))*emgSamplingFrequency);
       emgAnalyzed.ReachTimestamp(i,j) = reachpos/emgSamplingFrequency;
       emgAnalyzed.InitializeTimestamp(i,j) = initpos/emgSamplingFrequency;
-      emgAnalyzed.PeakValueRaw(i,j) = max(emgAnalyzed.RawAverage(i,j,initpos:end),[],3);
-      closestpeak = find(emgAnalyzed.RawAverage(i,j,initpos:end)==emgAnalyzed.PeakValueRaw(i,j),1);
+      emgAnalyzed.PeakValueRawAverage(i,j) = max(emgAnalyzed.RawAverage(i,j,initpos:end),[],3);
+      closestpeak = find(emgAnalyzed.RawAverage(i,j,initpos:end)==emgAnalyzed.PeakValueRawAverage(i,j),1);
       emgAnalyzed.ClosestPeakPosition(i,j) = closestpeak+initpos;
       emgAnalyzed.ClosestPeakTimestamp(i,j) = (closestpeak+initpos)/emgSamplingFrequency;
       emgAnalyzed.TimeToPeak(i,j) = closestpeak/emgSamplingFrequency;
     end
   end
 
-  % Computes area under curve for each annotation using the average
-  emgAnalyzed.AreaUnderCurve=trapz((0:size(emgAnalyzed.RawAverage,3)-1)/emgSamplingFrequency,emgAnalyzed.RawAverage,3);
+  % Computes area under curve for each annotation using normalized raw data
+  emgAnalyzed.AreaUnderCurveNormalized=trapz((0:size(emgAnalyzed.RawData,3)-1)/emgSamplingFrequency,...
+    (emgAnalyzed.RawData./emgAnalyzed.Baseline),3);
 
   % Compute the average voltage at each time point for all annotations
   emgAnalyzed.Average = sum(emgAnalyzed.RawData, 2)/size(emgAnalyzed.RawData,2);
-  % % Compute peak and peak time in the average signal
-  % emgAnalyzed.PeakValueAverage = max(emgAnalyzed.Average,[],3);
-  % for i = 1:size(emgAnalyzed.PeakValueAverage,1)
-  %   for j = 1:size(emgAnalyzed.PeakValueAverage,2)
-  %     peakpos = find(emgAnalyzed.Average(i,j,:)==emgAnalyzed.PeakValueAverage(i,j));
-  %     closestpeak = min(abs(peakpos-reachpos));
-  %     emgAnalyzed.ClosestPeakPositionAverage(i,j) = closestpeak;
-  %     emgAnalyzed.ClosestPeakTimestampAverage(i,j) = (closestpeak-reachpos)/emgSamplingFrequency+timestampEMG.Reach(j);
-  %   end
-  % end
-
 
   % Computes the moving average for a fixed window eg. 25 ms. Useful to find gradual trends in data
   emgAnalyzed.MovingAverage = movingAverage(emgAnalyzed.Average,movingAverageWindow,emgSamplingFrequency,3);
-  % % Compute peak and peak time in the average signal
-  % emgAnalyzed.PeakValueMovingAverage = max(emgAnalyzed.MovingAverage,[],3);
-  % for i = 1:size(emgAnalyzed.PeakValueMovingAverage,1)
-  %   for j = 1:size(emgAnalyzed.PeakValueMovingAverage,2)
-  %     peakpos = find(emgAnalyzed.MovingAverage(i,j,:)==emgAnalyzed.PeakValueMovingAverage(i,j));
-  %     closestpeak = min(abs(peakpos-reachpos));
-  %     emgAnalyzed.ClosestPeakPositionMovingAverage(i,j) = closestpeak;
-  %     emgAnalyzed.ClosestPeakTimestampMovingAverage(i,j) = (closestpeak-reachpos)/emgSamplingFrequency+timestampEMG.Reach(j);
-  %   end
-  % end
--- run_EMGAnnotationAnalysis.m
+++ run_EMGAnnotationAnalysis.m
@@ -1,20 +1,33 @@
 emgPathName = '/Users/chico/Downloads/emg/';
-fname = {'18LLR_OFF_annotations.xlsx',...
-	'18LLR_ONG_annotations.xlsx',...
-	'18LLR_ONI_annotations.xlsx',...
-	'22UN_OFF_annotations.xlsx',...
-	'22UN_ONG_annotations.xlsx',...
-	'22UN_ONI_annotations.xlsx'};
+fname = {'18LLR_OFF_20180606_annotations.xlsx',...
+	'18LLR_OFF_20180607_annotations.xlsx',...
+	'18LLR_OFF_20180608_annotations.xlsx',...
+	'18LLR_ONG_20180406_annotations.xlsx',...
+	'18LLR_ONG_20180606_annotations.xlsx',...
+	'18LLR_ONG_20180608_annotations.xlsx',...
+	'18LLR_ONI_20180406_annotations.xlsx',...
+	'18LLR_ONI_20180606_annotations.xlsx',...
+	'18LLR_ONI_20180607_annotations.xlsx',...
+	'18LLR_ONI_20180608_annotations.xlsx',...
+	'22UN_OFF_20180606_annotations.xlsx',...
+	'22UN_OFF_20180607_annotations.xlsx',...
+	'22UN_OFF_20180608_annotations.xlsx',...
+	'22UN_ONG_20180607_annotations.xlsx',...
+	'22UN_ONG_20180608_annotations.xlsx',...
+	'22UN_ONI_20180606_annotations.xlsx',...
+	'22UN_ONI_20180607_annotations.xlsx',...
+	'22UN_ONI_20180608_annotations.xlsx'};
 
 % fname = {'18LLR_ONG_annotations.xlsx'};
 
 % fname = {'18LLR_OFF_20180606_annotations.xlsx'};
 
 emgSamplingFrequency = 2000; %Hz
-videoSamplingFrequency = 250; %Hz
+videoSamplingFrequency = ;%; %Hz
 padding = 0.5; % 0.5s
 movingAverageWindow = 25/1000; % 25ms
 baselineEnd = 0.2; % 0.2s
+channels = 1:3;
 
 
 emgCounterReference = readtable('/Users/chico/Downloads/emg/annotations_18LLR_22UN_Master.txt');
@@ -34,17 +47,20 @@ for f = 1:length(fname)
 			% Assumption emg filename is export_18LLR_20180622.edf
 			emgFilename = fullfile(emgPathName,...
 			  ['export_',Animal{i},'_',num2str(Dates(j)),'.edf']);
-			emgDataAll = emgRetrieve(emgFilename);
 			% load('/Users/chico/Downloads/emg/18LLR_OFF_20180606_RawEMG_All.mat')
 			indx = find(strcmpi(annotations.Animal,Animal(i)) & annotations.Date==Dates(j));
 			[ignore,trialName] = fileparts(annotations.Filename{indx(1)});
 
-
+			% Retrieve emg data
+			emgDataAll = emgRetrieve(emgFilename);
+			save(fullfile(emgPathName,[trialName,'_RawEMGAll.mat']),'emgDataAll')
+			% Pick baseline
+			emgBaseline = emgDataAll(:,1:round(baselineEnd*emgSamplingFrequency));
 			% Select samples in the emg channel data that corresponds to the time stamp from the annotations data file
 			% Samples are chosen paddingDuration  before and after time stamp 
 			
 			[emg, ts] = emgExtractFromReach(emgDataAll,annotations(indx,:),emgCounterReference,videoSamplingFrequency,emgSamplingFrequency,padding);
-			out = emgDataAnalysis(emg,emgSamplingFrequency,ts);
+			out = emgDataAnalysis(emg(channels,:,:),emgBaseline,emgSamplingFrequency,ts);
 			if (i==1)&&(j==1)
 				emgData=emg;
 				timestampEMG=ts;
@@ -58,24 +74,34 @@ for f = 1:length(fname)
 				  emgAnalyzed = setfield(emgAnalyzed,aField,cat(2, getfield(emgAnalyzed, aField),getfield(out, aField)));
 				end
 			end
-			save(fullfile(emgPathName,[trialName,'_RawEMG.mat']),'emgData');
+			save(fullfile(emgPathName,[trialName,'_RawEMGWindow.mat']),'emgData');
 			save(fullfile(emgPathName,[trialName,'_AnalyzedEMG.mat']),'emgAnalyzed','timestampEMG');
+			a=[];
+	    fields = {'InitializeTimestamp','ReachTimestamp','PeakValueRawAverage','ClosestPeakPosition','ClosestPeakTimestamp','TimeToPeak','FoldChangeMean','AreaUnderCurveNormalized'};
+	    for k=1:length(fields)
+	    	a=cat(3,a,getfield(emgAnalyzed,fields{k}));
+	    end
+	    for k = channels
+	    	c=reshape(a(k,:,:),[size(a,2),size(a,3)]);
+	    	t = array2table(c,'VariableNames',fields);
+	    	writetable(t,fullfile(emgPathName,[trialName,'_EMGAnalysis.xlsx']),'Sheet',k)
+	    end
 
 			% h=figure('Name',trialName);
 			% subplot(3,1,1)
-			% plot((0:size(emgAnalyzed.Average,3)-1)/emgSamplingFrequency,reshape(emgAnalyzed.Average(1,1,:),1,2001))
+			% plot((0:size(emgAnalyzed.Average,3)-1)/emgSamplingFrequency,reshape(emgAnalyzed.Average(1,1,:),1,1601))
 			% hold on
-			% plot((0:size(emgAnalyzed.Average,3)-1)/emgSamplingFrequency,reshape(emgAnalyzed.MovingAverage(1,1,:),1,2001),'r')
+			% plot((0:size(emgAnalyzed.Average,3)-1)/emgSamplingFrequency,reshape(emgAnalyzed.MovingAverage(1,1,:),1,1601),'r')
 			% title('Channel1')
 			% subplot(3,1,2)
-			% plot((0:size(emgAnalyzed.Average,3)-1)/emgSamplingFrequency,reshape(emgAnalyzed.Average(2,1,:),1,2001))
+			% plot((0:size(emgAnalyzed.Average,3)-1)/emgSamplingFrequency,reshape(emgAnalyzed.Average(2,1,:),1,1601))
 			% hold on
-			% plot((0:size(emgAnalyzed.Average,3)-1)/emgSamplingFrequency,reshape(emgAnalyzed.MovingAverage(2,1,:),1,2001),'r')
+			% plot((0:size(emgAnalyzed.Average,3)-1)/emgSamplingFrequency,reshape(emgAnalyzed.MovingAverage(2,1,:),1,1601),'r')
 			% title('Channel2')
 			% subplot(3,1,3)
-			% plot((0:size(emgAnalyzed.Average,3)-1)/emgSamplingFrequency,reshape(emgAnalyzed.Average(3,1,:),1,2001))
+			% plot((0:size(emgAnalyzed.Average,3)-1)/emgSamplingFrequency,reshape(emgAnalyzed.Average(3,1,:),1,1601))
 			% hold on
-			% plot((0:size(emgAnalyzed.Average,3)-1)/emgSamplingFrequency,reshape(emgAnalyzed.MovingAverage(3,1,:),1,2001),'r')
+			% plot((0:size(emgAnalyzed.Average,3)-1)/emgSamplingFrequency,reshape(emgAnalyzed.MovingAverage(3,1,:),1,1601),'r')
 			% title('Channel3')
 			% hold off
 		end
