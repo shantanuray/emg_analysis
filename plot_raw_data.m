6faa0691c5ebe72da6eecafe826cc98af23240a0
Shantanu Ray <chico.chego@gmail.com>
2018-08-12 16:39:55 +0800

    Plotting actual. Updated title

--- plot_raw_data.m
+++ plot_raw_data.m
@@ -1,14 +1,16 @@
-channel = 1;
-reach_num = [5,8,10,2];
+channel = 2;
+reach_num = 1:8;
 
-emg = reshape(emgAnalyzed.RawAverage(channel,:,:),size(emgAnalyzed.RawAverage,2),size(emgAnalyzed.RawAverage,3));
-figure
+emgRaw = reshape(emgAnalyzed.RawData(channel,:,:),size(emgAnalyzed.RawData,2),size(emgAnalyzed.RawData,3));
+emgAvg = reshape(emgAnalyzed.RawAverage(channel,:,:),size(emgAnalyzed.RawAverage,2),size(emgAnalyzed.RawAverage,3));
+h=figure('Name',['FPS: 250; Channel: ',num2str(channel)]);
 j = 1;
 for i = reach_num
   subplot(length(reach_num),1,j)
   j = j +1;
-  plot(emg(i,:)/mean(emg(i,:)))
+  plot(emgRaw(i,:),'b-')
   hold on
+  plot(emgAvg(i,:),'r-')
   %plot(reshape(emgAnalyzed.MovingAverage(3,1,:),1,1601)/mean(reshape(emgAnalyzed.MovingAverage(3,1,:),1,1601)),'r-');
   %plot(reshape(emgAnalyzed.Average(3,1,:),1,1601)/mean(reshape(emgAnalyzed.Average(3,1,:),1,1601)),'b-');
 end
