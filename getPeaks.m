4eeff49cc5f4c2c9da2e2fcf45bb4d62fb352099
Shantanu Ray <shantanu.ray@nexsales.com>
2021-09-07 18:39:01 -0700

    Wrapper for find_peaks

---
+++ getPeaks.m
@@ -0,0 +1,34 @@
+function [peakAmplitude, peakLocation] = getPeaks(data, fs, varargin)
+% [peakAmplitude, peakLocation] = getPeaks(data, fs,
+%            			                   'minPeakDistance', 100/1000,
+%            			                   'rmsPctCutoff', 0.25);
+% Wrapper around find_peaks.m
+	pkg load signal % Load signal processing library
+
+	p = readInput(varargin);
+    [minPeakDistance, rmsPctCutoff] = parseInput(p.Results);
+
+	data_rms = rms(data);
+	if isnan(rmsPctCutoff)
+		[peakAmplitude, peakLocation] = findpeaks(data, 'MinPeakDistance', round(minPeakDistance*fs));
+	else
+		[peakAmplitude, peakLocation] = findpeaks(data, 'MinPeakDistance', round(minPeakDistance*fs), 'MinPeakHeight', rmsPctCutoff*data_rms);		
+	endif
+    %% Read input
+    function p = readInput(input)
+        %   - minPeakDistance     Default - 100/1000; % 100ms
+        %   - rmsPctCutoff        Default - NaN (skip this filter)
+        p = inputParser;
+        minPeakDistance = 100/1000;
+        rmsPctCutoff = NaN;
+
+        addParameter(p,'minPeakDistance',minPeakDistance, @isnumeric);
+        addParameter(p,'rmsPctCutoff',rmsPctCutoff, @isnumeric);
+        parse(p, input{:});
+    end
+
+    function [minPeakDistance, rmsPctCutoff] = parseInput(p)
+        minPeakDistance = p.minPeakDistance;
+        rmsPctCutoff = p.rmsPctCutoff;
+    end
+end
