5ec9980dcbd78f5e867e1ef69ca716a8af83beee
Shantanu Ray <shantanu.ray@nexsales.com>
2021-09-07 18:55:18 -0700

    Replicate EMG data sans raw

--- emgGetPeaksFolder.m
+++ emgGetPeaksFolder.m
@@ -1,17 +1,30 @@
-function [emgData, peakAnaMetrics, peakDistAct] = emgGetPeaksFolder(emgData, varargin)
-    % emgData = emgGetPeaksFolder(emgData, channels, segments);
-    % emgData  - EMG data for entire folder - for each channel, filtered and separated into segments
+function [peakData, peakMetrics, peakDistances] = emgGetPeaksFolder(emgData, varargin)
+    % peakData = emgGetPeaksFolder(emgData, channels, segments);
+    % [peakData, peakMetrics, peakDistances] = emgGetPeaksFolder(emgData);
+    % Run get_peaks on filtered EMG signal for entire folder
+    %			  for each channel, filtered and separated into segments
+    % emgData	- EMG data with raw signal
+    % peakData  - Replicates structure of EMG data sans the actual raw data
+    % peakMetrics - double [# files in folder, # channels, # segments, # metrics]
+    %			   Metrics	[averageFrequency, averagePeakDistance, averagePeakAmplitude,
+    %						 peakDistanceStdDev, peakAmplitudeStdDev]
+    % peakDistances - cell {# channels, # segments} with peak distances from all the files for (chan,seg)
+    %
+    % Parameters:
     % channels - {'bi', 'tri', 'ecu', 'trap'}
     % segments - {'discrete', 'rhythmic'}
-    % Wrapper around getPeaks.m for entire folder data
+    % minPeakDistance - for find_peaks: 100ms (0.1)
+    % rmsPctCutoff - RMS cut off for min peak height: Default skip (NaN)
 
     p = readInput(varargin);
     [channels, segments, minPeakDistance, rmsPctCutoff] = parseInput(p.Results);
-    peakAnaMetrics = NaN(length(emgData), length(channels), length(segments), 5);
-    peakDistAct = cell(length(channels), length(segments));
+    peakMetrics = NaN(length(emgData), length(channels), length(segments), 5);
+    peakDistances = cell(length(channels), length(segments));
+    peakData = emgData;
 
 	for i = 1:length(emgData)
 		for j = 1:length(channels)
+			peakData(i).(channels{j}) = rmfield(peakData(i).(channels{j}), 'raw');
 			if ~isempty(emgData(i).(channels{j}))
 				for k = 1:length(segments)
 					if isfield(emgData(i).(channels{j}), segments{k})
@@ -19,6 +32,7 @@ function [emgData, peakAnaMetrics, peakDistAct] = emgGetPeaksFolder(emgData, var
 							disp(sprintf('Get peaks for %s channel %s segment %s', emgData(i).fileID, channels{j}, segments{k}))
 							fs = emgData(i).(channels{j}).samplingFrequency;
 							data = emgData(i).(channels{j}).(segments{k}).raw;
+							peakData(i).(channels{j}).(segments{k}) = rmfield(peakData(i).(channels{j}).(segments{k}), 'raw');
 							% rectify bipolar emg signals
 							data = abs(data);
 							% Moving average filter
@@ -33,8 +47,8 @@ function [emgData, peakAnaMetrics, peakDistAct] = emgGetPeaksFolder(emgData, var
 							emgData(i).(channels{j}).(segments{k}).averagePeakAmplitude = pk_amp;
 							emgData(i).(channels{j}).(segments{k}).peakDistanceStdDev = pk_dist_std;
 							emgData(i).(channels{j}).(segments{k}).peakAmplitudeStdDev = pk_amp_std;
-							peakAnaMetrics(i,j,k,:) = [pk_freq, pk_dist, pk_amp, pk_dist_std, pk_amp_std];
-							peakDistAct{j,k} = [peakDistAct{j,k};(idx(2:end)-idx(1:end-1))/fs];
+							peakMetrics(i,j,k,:) = [pk_freq, pk_dist, pk_amp, pk_dist_std, pk_amp_std];
+							peakDistances{j,k} = [peakDistances{j,k};(idx(2:end)-idx(1:end-1))/fs];
 						end
 					endif
 				endfor
