a4c93f6ad17553d9d932517cb4bcd823f7d18f6f
Shantanu Ray <chico.chego@gmail.com>
2022-08-23 09:47:00 -0700

    Consolidate peak analysis and write to csv

---
+++ emg_ropepull/writePeakAnalysisToFile.m
@@ -0,0 +1,11 @@
+function writePeakAnalysisToFile(peakData, fileID, channels, segments, saveloc)
+	if nargin < 5
+		saveloc = pwd;
+	end
+	fileIDParts = strsplit(fileID, fileDelim);
+	[peakDistTable, peakAmplTable] = flattenEMGPeakAnalysis(peakData, fileIDParts{1}, [],...
+															'channels', channels,...
+															'segments', segments);
+	writetable(peakDistTable, fullfile(saveloc, sprintf('%s_peak_dist.csv', fileID));
+	writetable(peakAmplTable, fullfile(saveloc, sprintf('%s_peak_amp.csv', fileID));
+end
